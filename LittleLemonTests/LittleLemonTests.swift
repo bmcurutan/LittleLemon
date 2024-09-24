d37499f1ba36f8f325cf7cdfd681d51df4c591bb
Bianca <bmcurutan@gmail.com>
2024-09-24 14:10:01 -0700

    Initial Commit

---
+++ LittleLemonTests/LittleLemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonTests.swift
+//  LittleLemonTests
+//
+//  Created by Bianca Curutan on 9/24/24.
+//
+
+import Testing
+@testable import LittleLemon
+
+struct LittleLemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
